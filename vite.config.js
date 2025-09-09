ec6a8be3e53d49b1b18a7891672628d0d0958d92
shohei909 <909mmsphere@gmail.com>
2025-09-09 14:27:50 +0900

    bootstrap を使用してUIを実装

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,29 @@
-// vite.config.js
-export default {
-  root: 'pages'
-}
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	base: "./",
+	root: "src",
+	publicDir: resolve(__dirname, "public"),
+	build: {
+		// distフォルダに出力
+		outDir: resolve(__dirname, "dist"),
+		// 存在しないときはフォルダを作成する
+		emptyOutDir: true,
+		copyPublicDir: true,
+		rollupOptions: {
+			// entry pointがあるindex.htmlのパス
+			input: {
+				"": resolve(__dirname, "src/index.html"),
+			},
+			// bundle.jsを差し替えする
+			output: {
+				entryFileNames: "assets/bundle.js",
+			},
+		},
+	},
+	server: {
+		port: 8080
+	}
+});
