9b214501c325c4a58fb7b6a1cfa80e560626389c
shohei909 <909mmsphere@gmail.com>
2025-09-16 10:07:58 +0900

    worker の使いまわしを実装

--- src/js/solver/packing_ver_dlx.ts
+++ src/js/solver/packing_ver_dlx.ts
@@ -1,6 +1,7 @@
 import { usingMarker } from "./solver_root";
 
 let dlxWorkers: Worker[] = [];
+let workerPool: Worker[] = [];
 
 export async function startPacking_dlx(
     currentMarker: null | {},
@@ -8,14 +9,19 @@ export async function startPacking_dlx(
     onSolved: (solution: PackingSolution) => void,
     onFinished: () => void
 ) {
-    let dlxWorker = new Worker(new URL('./packing_ver_dlx_worker.ts', import.meta.url), { type: 'module' });
+    let dlxWorker;
+    if (workerPool.length > 0) {
+        dlxWorker = workerPool.pop()!;
+    } else {
+        dlxWorker = new Worker(new URL('./packing_ver_dlx_worker.ts', import.meta.url), { type: 'module' });
+    }
     dlxWorker.onmessage = (event) => {
         if (currentMarker != usingMarker) { return; }
         if (event.data.type === "solution") {
             onSolved(event.data.solution);
         } else if (event.data.type === "finished") {
-            dlxWorker?.terminate();
             dlxWorkers.splice(dlxWorkers.indexOf(dlxWorker), 1);
+            workerPool.push(dlxWorker);
             onFinished();
         }
     };
@@ -26,5 +32,9 @@ export async function abort_dlx_worker() {
     for (const worker of dlxWorkers) {
         worker.terminate();
     }
+    for (const worker of workerPool) {
+        worker.terminate();
+    }
     dlxWorkers = [];
+    workerPool = [];
 }
